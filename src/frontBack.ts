d2d0c7571f741ef060e7bbe7880a24818cabb0e1
Aleksandr Dremov <dremov.me@gmail.com>
2021-11-27 02:17:40 +0300

    Typescript Railways

--- src/frontBack.ts
+++ src/frontBack.ts
@@ -1,76 +1,17 @@
-import ymaps, {driving, masstransit} from "yandex-maps";
-
-interface WayPoint {
-    name: string
-}
-
-interface GeoLocation {
-    lat: number,
-    lon: number
-}
-
-enum TransportType {
-    train,
-    publicTransport = "masstransit",
-    car = "auto",
-    pedestrian = "pedestrian"
-}
-
-interface RequestParams {
-    datetime: Date
-    exclusions: Set<TransportType>
-}
-
-class MultiRoute {
-    duration: number = 0
-    distance: number = 0
-    price: number = 0
-    hiddenPrice: number = 0
-
-    path: Route[] = []
-
-    constructor() {
-
-    }
-
-    private calcParameters() {
-
-    }
-}
-
-interface RoutePoint {
-    coordinates: { lat: number, lon: number }
-    name: string
-}
-
-interface Route {
-    duration: number
-    distance: number
-    price: number
-    hiddenPrice: number
-
-    startPoint: RoutePoint
-    endPoint: RoutePoint
-    info: object
-}
-
-class PossibleRoutes {
-    fast?: MultiRoute
-    cheap?: MultiRoute
-    best?: MultiRoute
-
-    others: MultiRoute[] = []
-
-    constructor(allRoutes: MultiRoute[]) {
-
-    }
-}
+import ymaps from "yandex-maps";
+import {
+    WayPoint
+    GeoLocation,
+    PossibleRoutes,
+    RequestParams,
+    TransportType,
+    YAPIRoute,
+    YAPISearchControlEntity
+} from "./dataTypes";
 
 function getRoutes(waypoints: WayPoint[], params: RequestParams): PossibleRoutes {
 
 }
-
-type YAPIRoute = ymaps.multiRouter.driving.Route | ymaps.multiRouter.masstransit.Route | object
 
 function getYAMultiRoutes(waypoints: WayPoint[], params: RequestParams): YAPIRoute[] {
     return [TransportType.car, TransportType.publicTransport, TransportType.pedestrian]
@@ -85,31 +26,41 @@ function getYAMultiRoutes(waypoints: WayPoint[], params: RequestParams): YAPIRou
 }
 
 /**
- *
+ * Get railway stations nearby userPoint location
  * @param userPoint user's location
  * @param range in degrees
  */
-function getStations(userPoint: GeoLocation, range: number): YAPI /* TODO */ {
+function getStations(userPoint: GeoLocation, range: number): YAPISearchControlEntity[] {
     const searchControl = new ymaps.control.SearchControl({
         options: {
             provider: 'yandex#search',
-            boundedBy: [[55.918502, 37.517399], [55.937514, 37.533705]]
+            boundedBy: [[userPoint.lat - range, userPoint.lon - range], [userPoint.lat + range, userPoint.lon + range]]
         }
     });
 
-    myMap.controls.add(searchControl);
+    const namesDiscriminator = new Set<string>(['железнодорож', 'вокзал', 'станция'])
+    const findStations = new Set<string>(['железнодорожная', 'станция', 'платформа'])
+    let allResults: YAPISearchControlEntity[] = []
 
-    // Программно выполним поиск определённых кафе в текущей
-    // прямоугольной области карты.
+    for (const searchString of findStations) {
+        searchControl.search(searchString).then(function () {
+            const geoObjectsArray = searchControl.getResultsArray();
+            allResults = allResults.concat(geoObjectsArray as YAPISearchControlEntity[])
+        });
+    }
 
-
-    searchControl.search('станция').then(function () {
-        var geoObjectsArray = searchControl.getResultsArray();
-        if (geoObjectsArray.length) {
-            // Выводит свойство name первого геообъекта из результатов запроса.
-            console.log(geoObjectsArray);
+    allResults = allResults.filter((element, index) => {
+        for(const discriminator of namesDiscriminator) {
+            for(const catWord of element.properties._data.categoriesText.split(' ')){
+                if(catWord.toLowerCase().includes(discriminator.toLowerCase())) {
+                    return true
+                }
+            }
         }
-    });
+        return false
+    })
+
+    return allResults;
 }
 
 /**
